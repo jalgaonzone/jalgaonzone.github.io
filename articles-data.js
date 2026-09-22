2d0de1c8ecc2ddbdbef534700cf45b804c82cd5c
jalgaonzone <yashsananse21@gmail.com>
2026-09-22 22:45:09 +0530

    Create articles-data.js

---
+++ articles-data.js
@@ -0,0 +1,30 @@
+const articles = [
+
+    {
+        id: "oxygen",
+        title: "पाळधीत ऑक्सिजन पार्कचे भूमिपूजन",
+        category: "Jalgaon News",
+        image: "oxygen-park-newspng.jpg",
+        description: "जळगाव जिल्ह्यातील पाळधी येथे ऑक्सिजन पार्कच्या भूमिपूजनाची माहिती.",
+        link: "article.html?id=oxygen"
+    },
+
+    {
+        id: "agriculture",
+        title: "शेतकऱ्यांसाठी कृषी अनुदानाची माहिती",
+        category: "Agriculture",
+        image: "agriculture.png",
+        description: "जळगाव जिल्ह्यातील शेती आणि कृषी क्षेत्राशी संबंधित महत्त्वाची माहिती.",
+        link: "article.html?id=agriculture"
+    },
+
+    {
+        id: "history",
+        title: "जळगाव जिल्ह्याची माहिती आणि इतिहास",
+        category: "History",
+        image: "Jalgaon city.jpeg",
+        description: "जळगाव जिल्ह्याचा इतिहास, भौगोलिक माहिती आणि महत्त्वाची माहिती.",
+        link: "article.html?id=history"
+    }
+
+];
